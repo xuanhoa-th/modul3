09fd0b318d48006976e309218850bb8b9cfbdd7e
my_app <you@example.com>
2020-05-22 13:30:57 +0700

    ss

---
+++ task_manager/resources/views/create.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+
+
+    <title>Document</title>
+
+</head>
+<body>
+<div class="container">
+    <h3>Them moi </h3>
+    <form method="post" action="">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Họ và tên</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Số điện thoại</label>
+            <input type="number" class="form-control" id="exampleInputPassword1">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputPassword1">Email</label>
+            <input type="number" class="form-control" id="exampleInputPassword1">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
--- task_manager/routes/web.php
+++ task_manager/routes/web.php
@@ -20,4 +20,27 @@ Route::prefix('customer')->group(function (){
     Route::get('index', function (){
         return view('index');
     });
+    Route::get('create', function () {
+        return view('create');
+    });
+//
+    Route::post('store', function () {
+
+    });
+
+    Route::get('{id}/show', function () {
+        // Hiển thị thông tin chi tiết khách hàng có mã định danh id
+    });
+
+    Route::get('{id}/edit', function () {
+        // Hiển thị Form chỉnh sửa thông tin khách hàng
+    });
+
+    Route::patch('{id}/update', function () {
+        // xử lý lưu dữ liệu thông tin khách hàng được chỉnh sửa thông qua PATCH từ form
+    });
+
+    Route::delete('{id}', function () {
+        // Xóa thông tin dữ liệu khách hàng
+    });
 });
